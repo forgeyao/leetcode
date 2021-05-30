c571522dcfd2c1a80307331955ef6fb60a87b58e
Forge Yao <forgeryao@ForgerdeMacBook-Pro.local>
2021-05-30 23:54:41 +0800

    commit 131

--- 131-partition/131-partition.go
+++ 131-partition/131-partition.go
@@ -1,3 +1,6 @@
+/**
+* https://leetcode-cn.com/problems/palindrome-partitioning/
+ */
 package main
 
 import "fmt"
@@ -12,48 +15,42 @@ func partition(s string) [][]string {
 		return true
 	}
 
-	part := func(s string) [][]string {
-		res := make([][]string, 16)
+	var part func(s string) [][]string
+	part = func(s string) [][]string {
+		var res [][]string
 		if isPalindrome(s) {
-			ss := make([]string, 1)
-			ss = append(ss, s)
+			ss := []string{s}
 			res = append(res, ss)
 		}
 		if len(s) == 1 {
 			return res
 		}
 
-		sub := part(s[1:len(s)])
-		for i := 0; i < len(sub); i++ {
-			sub[i] = append([]string{string(s[0])}, sub[i])
-		}
-		return append(res, sub)
-	}
+		for i := 1; i < len(s); i++ {
+			left, right := s[0:i], s[i:len(s)]
 
-	res := make([][]string, 16)
-	for i := 0; i < len(s); i++ {
-		left, right := "", ""
-		if i == 0 {
-			left = ""
-			right = s
-		} else {
-			left = s[0:i]
-			right = s[i:len(s)]
-		}
-
-		leftSub := part(left)
-		rightSub := part(right)
-		for j := 0; j < len(leftSub); j++ {
+			if !isPalindrome(left) {
+				continue
+			}
+			//fmt.Println("left:", left)
+			rightSub := part(right)
+			//fmt.Println("right:", rightSub)
 			for k := 0; k < len(rightSub); k++ {
-				res = append(res, leftSub[j], rightSub[k])
+				ss := []string{left}
+				ss = append(ss, rightSub[k]...)
+				res = append(res, ss)
 			}
 		}
+		return res
 	}
+
+	var res [][]string
+	res = part(s)
 	return res
 }
 
 func main() {
-	s := []string{"aab", "a"}
+	s := []string{"a", "aa", "ab", "aab", "aaaaa", "aababa"}
 	for i := 0; i < len(s); i++ {
 		fmt.Println("ret:", partition(s[i]))
 	}
