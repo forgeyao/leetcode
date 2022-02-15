c939e20d994352a10798462fd2bf3fb312266cc5
Forge <fguilq@gmail.com>
2022-02-15 09:52:50 +0800

    update 1380

--- 1380/1380-luckyNumbers.go
+++ 1380/1380-luckyNumbers.go
@@ -3,6 +3,10 @@ package main
 
 import "fmt"
 
+/**
+ * 时间O(m*n),m 是行数,n 是列数
+ * 空间O(m), 缓存行里最小值
+ */
 func luckyNumbers(matrix [][]int) []int {
 	col := len(matrix)
 	row := len(matrix[0])
@@ -31,11 +35,42 @@ func luckyNumbers(matrix [][]int) []int {
 	return ret
 }
 
+/**
+ * 优化掉空间
+ * 时间O(m*n),m 是行数,n 是列数
+ * 空间O(1)
+ */
+func luckyNumbers2(matrix [][]int) []int {
+	ret := []int{}
+	for i := 0; i < len(matrix); i++ {
+		minJ := 0
+		for j := 0; j < len(matrix[i]); j++ {
+			if matrix[i][j] < matrix[i][minJ] {
+				minJ = j
+			}
+		}
+		b := false
+		for k := 0; k < len(matrix); k++ {
+			if matrix[i][minJ] < matrix[k][minJ] {
+				b = false
+				break
+			}
+			b = true
+		}
+		if b {
+			ret = append(ret, matrix[i][minJ])
+		}
+	}
+	return ret
+}
+
 func main() {
-	n := [][]int{{3, 7, 8}, {9, 11, 13}, {15, 16, 17}}
-	fmt.Println("ret:", luckyNumbers(n))
-	n2 := [][]int{{1, 10, 4, 2}, {9, 3, 8, 7}, {15, 16, 17, 12}}
-	fmt.Println("ret:", luckyNumbers(n2))
-	n3 := [][]int{{7, 8}, {1, 2}}
-	fmt.Println("ret:", luckyNumbers(n3))
+	ns := [][][]int{
+		{{3, 7, 8}, {9, 11, 13}, {15, 16, 17}},
+		{{1, 10, 4, 2}, {9, 3, 8, 7}, {15, 16, 17, 12}},
+		{{7, 8}, {1, 2}},
+	}
+	for _, n := range ns {
+		fmt.Println("ret:", luckyNumbers(n), luckyNumbers2(n))
+	}
 }
