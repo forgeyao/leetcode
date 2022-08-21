1d9db9aa59ce74f55e003ebd399e002b5648f8be
Forge Yao <fguilq@gmail.com>
2022-08-21 12:19:51 +0800

    update 20

--- 1-200/20-isValid/20-isValid.go
+++ 1-200/20-isValid/20-isValid.go
@@ -13,23 +13,22 @@ func isValid(s string) bool {
 		if s[i] == '(' || s[i] == '{' || s[i] == '[' {
 			arr = append(arr, s[i])
 		} else {
-			if len(arr) < 1 {
+			ls := len(arr) - 1
+			if ls >= 0 && (arr[ls] == '(' && s[i] == ')' || arr[ls] == '{' && s[i] == '}' || arr[ls] == '[' && s[i] == ']') {
+				arr = arr[:ls]
+			} else {
 				return false
 			}
-			c := arr[len(arr)-1]
-			if c == '(' && s[i] == ')' || c == '{' && s[i] == '}' || c == '[' && s[i] == ']' {
-				arr = arr[:len(arr)-1]
-				continue
-			}
-			return false
 		}
 	}
 	return len(arr) == 0
 }
 
 func main() {
-	s := []string{"()", "()[]{}", "(]", "([)]", "{[]}", "((("}
-	for i := 0; i < len(s); i++ {
-		fmt.Println("ret:", isValid(s[i]))
+	s := []string{"()", "()[]{}", "(]", "([)]", "{[]}", "(((", "(){}}{", "(]"}
+	ans := []bool{true, true, false, false, true, false, false, false}
+	for i := 0; i < len(s) && i < len(ans); i++ {
+		r := isValid(s[i])
+		fmt.Println(s[i], "ret:", r, ans[i])
 	}
 }
