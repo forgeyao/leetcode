474aa15566b408a15f7335d7b283d9fa04878799
Forge Yao <fguilq@gmail.com>
2022-10-25 09:30:27 +0800

    update 88

--- 1-200/88/88-merge.go
+++ 1-200/88/88-merge.go
@@ -3,13 +3,13 @@ package main
 
 import "fmt"
 
+/**
+ * 时间 O(n)
+ * 空间 O(1)
+ */
 func merge(nums1 []int, m int, nums2 []int, n int) {
-	for i, j, k := m-1, n-1, len(nums1)-1; k >= 0; k-- {
-		if j < 0 {
-			break
-		}
-
-		if i >= 0 && nums1[i] >= nums2[j] {
+	for i, j, k := m-1, n-1, len(nums1)-1; j >= 0; k-- {
+		if i >= 0 && nums1[i] > nums2[j] {
 			nums1[k] = nums1[i]
 			i--
 		} else {
@@ -20,10 +20,10 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 }
 
 func main() {
-	nums1 := [][]int{{1, 2, 3, 0, 0, 0}, {1}}
-	nums2 := [][]int{{2, 5, 6}, {}}
-	m := []int{3, 1}
-	n := []int{3, 0}
+	nums1 := [][]int{{1, 2, 3, 0, 0, 0}, {1}, {0}}
+	nums2 := [][]int{{2, 5, 6}, {}, {1}}
+	m := []int{3, 1, 0}
+	n := []int{3, 0, 1}
 	for i := 0; i < len(nums1) && i < len(nums2) && i < len(m) && i < len(n); i++ {
 		merge(nums1[i], m[i], nums2[i], n[i])
 		fmt.Println("ret:", nums1[i])
